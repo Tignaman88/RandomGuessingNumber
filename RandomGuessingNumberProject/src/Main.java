2ba9d3680cb9a74d6a37d1434c20cb1974f3e9c2
Tignaman88 <alessioscarpello2809@gmail.com>
2025-04-26 08:45:27 +0100

    Comparing Guessing To Random Number Achieved

--- RandomGuessingNumberProject/src/Main.java
+++ RandomGuessingNumberProject/src/Main.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        int score = 0;
+        int userGuessing;
 
         // Generate a random number between 0 and 10
         RandomNumber rn = new RandomNumber();
         int random = rn.randomNumber;
 
+        System.out.println("Hi! Try to guess the number between 0 and 10!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number between 0 and 10: ");
+        String guessing = scanner.nextLine();
+        userGuessing = Integer.parseInt(guessing);
+
+        if (userGuessing == random) {
+            System.out.println("Great job! You guessed correct!");
+            score++;
+        }
+        
 
     }
 }
