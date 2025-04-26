14d91269f7b635ac698f759dacf1af68069c7996
Tignaman88 <alessioscarpello2809@gmail.com>
2025-04-26 08:34:32 +0100

    Random Number Generated
    Between 0 and 10

--- RandomGuessingNumberProject/src/Main.java
+++ RandomGuessingNumberProject/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Generate a random number between 0 and 10
+        RandomNumber rn = new RandomNumber();
+        int random = rn.randomNumber;
+
+
     }
 }
---
+++ RandomGuessingNumberProject/src/RandomNumber.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class RandomNumber {
+
+    // Generate a random number between 0 and 10
+    Random random = new Random();
+    int randomNumber = random.nextInt(11);
+}
