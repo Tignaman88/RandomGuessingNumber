71e4c581bc4f1444163c20b907ebfc12cb90b27b
Tignaman88 <alessioscarpello2809@gmail.com>
2025-04-28 18:56:00 +0100

    Checks Working Half Way

--- RandomGuessingNumberProject/src/Main.java
+++ RandomGuessingNumberProject/src/Main.java
@@ -16,22 +16,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter a number between 0 and 10: ");
-        String guessing = scanner.nextLine();
-        userGuessing = Integer.parseInt(guessing);
 
-        if (isNumeric(guessing)) {
-            // Compare user's guessing to random number
-            userInput.userNumberToCompareWithRandomNumber(userGuessing, random, score);
-        } 
+        while (true) {
+            String guessing = scanner.nextLine();
+            userGuessing = Integer.parseInt(guessing);
+            if (!isNumeric(guessing)) {
+                System.out.println("You have not entered a number, please try again: ");
+            } else {
+                if (isNumeric(guessing)) {
+                    // Compare user's guessing to random number
+                    userInput.userNumberToCompareWithRandomNumber(userGuessing, random, score);
+                } else if (guessing.equalsIgnoreCase("q")) {
+                    break;
+                }
+            }
+        }
+
 
 
 
 
     }
 
-    public static boolean isNumeric(String str) {
+    public static boolean isNumeric(String userGuess) {
         try {
-            Integer.parseInt(str);
+            Integer.parseInt(userGuess);
             return true;
         } catch(NumberFormatException e){
             return false;
