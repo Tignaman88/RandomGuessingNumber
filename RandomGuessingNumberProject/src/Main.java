0122e7014ab9ef5ea837798fb20b06746e9e611f
Tignaman88 <alessioscarpello2809@gmail.com>
2025-05-19 18:49:31 +0100

    Conditions Added
    Conditions to check the user's help added

--- RandomGuessingNumberProject/src/Main.java
+++ RandomGuessingNumberProject/src/Main.java
@@ -5,55 +5,118 @@ public class Main {
 
         int score = 0;
         int userGuessing;
-        int userTries = 0;
+        int checkUserAnswer;
 
 
 
 
         CheckUserInputWithRandomNumber userInput = new CheckUserInputWithRandomNumber();
 
-        System.out.println("Hi! Try to guess the number between 0 and 10!");
+        System.out.println("Hi! Try to guess the number between 0 and 100!");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter a number between 0 and 10: ");
+        System.out.println("Please enter a number between 0 and 100: ");
 
-        while (true) {
+        boolean isGameOn = true;
+        boolean isRightGuess = true;
+        String wantToPlayAgain;
 
-            // Generate a random number between 0 and 10
-            RandomNumber rn = new RandomNumber();
-            int random;
-            random = rn.randomNumber;
+        while (isGameOn) {
+
+            // Generate a random number between 0 and 100
+            int random = (int) (Math.random() * 101);
+            System.out.println(random);
+
+            while (isRightGuess) {
 
 
-            String guessing = scanner.nextLine();
+                String guessing = scanner.nextLine();
+                userGuessing = Integer.parseInt(guessing);
 
-            if (guessing.equalsIgnoreCase("q")) break;
+                if (!isNumeric(guessing)) {
+                    System.out.println("You haven't entered a number, please try again: ");
+                } else {
 
-            if (!isNumeric(guessing) || Integer.parseInt(guessing) > 10 || Integer.parseInt(guessing) < 0) {
-                System.out.println("You have not entered a number, or the number entered is" +
-                        " less than 0 or greater than 10, please try again: ");
-
-            } else {
-                    // Compare user's guessing to random number
-                    userGuessing = Integer.parseInt(guessing);
                     if (userInput.userNumberToCompareWithRandomNumber(userGuessing, random)) {
-                        score++;
+
+                    score++;
+                    isRightGuess = false;
+                    System.out.println("Would you like to play again: if yes, press Y, press N for no: ");
+                    wantToPlayAgain = scanner.nextLine();
+                    if (wantToPlayAgain.equalsIgnoreCase("n")) {
+                        isGameOn = false;
+                    } else if (wantToPlayAgain.equalsIgnoreCase("y")){
+                        random = (int) (Math.random() * 101);
+                        System.out.println(random);
+                        isRightGuess = true;
                     }
-                userTries++;
 
+                } else {
+                    if (random < userGuessing) {
+                        checkUserAnswer = userGuessing - random;
+                        if (checkUserAnswer <= 10) {
+                            System.out.println("So Close!");
+                        } else if (checkUserAnswer <= 20) {
+                            System.out.println("Almost!");
+                        } else {
+                            System.out.println("Not Even Close!");
+                        }
+                    }
+                    if (random > userGuessing) {
+                        checkUserAnswer = random - userGuessing;
+                        if (checkUserAnswer <= 10) {
+                            System.out.println("So Close!");
+                        } else if (checkUserAnswer <= 20) {
+                            System.out.println("Almost!");
+                        } else {
+                            System.out.println("Not Even Close!");
+                        }
+                    }
+
+                    }
+                }
             }
-
-
-
-            }
-
-
-
-        System.out.println("You guessed " + score + " out of " + userTries + " guessing!");
-
-        if (score == userTries && score > 0) {
-            System.out.println("You guessed all numbers, well done!");
         }
+
+//        while (true) {
+//
+//            // Generate a random number between 0 and 10
+//            RandomNumber rn = new RandomNumber();
+//            int random;
+//            random = rn.randomNumber;
+//            System.out.println(random);
+//
+//
+//            String guessing = scanner.nextLine();
+//
+//            if (guessing.equalsIgnoreCase("q")) break;
+//
+//            if (!isNumeric(guessing) || Integer.parseInt(guessing) > 100 || Integer.parseInt(guessing) < 0) {
+//                System.out.println("You have not entered a number, or the number entered is" +
+//                        " less than 0 or greater than 10, please try again: ");
+//
+//            } else {
+//                    // Compare user's guessing to random number
+//                    userGuessing = Integer.parseInt(guessing);
+//                    if (userInput.userNumberToCompareWithRandomNumber(userGuessing, random)) {
+//                        score++;
+//                    } else if (random - userGuessing < 0 || random - userGuessing > 20) {
+//                        System.out.println("Acqua");
+//                    } else if (random - userGuessing <= 10) {
+//                        System.out.println("Fuoco");
+//                    } else System.out.println("Fuochino");
+
+//
+//            }
+//
+//
+//
+//            }
+
+
+
+        System.out.println("You guessed " + score + " numbers!");
+
 
 
 
--- RandomGuessingNumberProject/src/RandomNumber.java
+++ RandomGuessingNumberProject/src/RandomNumber.java
@@ -3,7 +3,6 @@ import java.util.Random;
 public class RandomNumber {
 
     // Generate a random number between 0 and 10
-    Random random = new Random();
-    int randomNumber = random.nextInt(11);
+    int randomNumber = (int) (Math.random() * 101);
 
 }
